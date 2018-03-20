f7695e36d15871ad6de5d0e1b44385d9444a269d
Alfonso <ajzuniga1@miuandes.cl>
2018-03-20 12:29:17 -0300

    Lab01

--- app/src/main/java/com/example/alfonso/lab01/LoginActivity.java
+++ app/src/main/java/com/example/alfonso/lab01/LoginActivity.java
@@ -3,6 +3,9 @@ package com.example.alfonso.lab01;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -28,6 +31,7 @@ import android.widget.AutoCompleteTextView;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -160,11 +164,11 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
         View focusView = null;
 
         // Check for a valid password, if the user entered one.
-        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
+        /*if (!TextUtils.isEmpty(password) && !isPasswordValid(password)) {
             mPasswordView.setError(getString(R.string.error_invalid_password));
             focusView = mPasswordView;
             cancel = true;
-        }
+        }*/
 
         // Check for a valid email address.
         if (TextUtils.isEmpty(email)) {
@@ -181,12 +185,27 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             // There was an error; don't attempt login and focus the first
             // form field with an error.
             focusView.requestFocus();
+            Context context = getApplicationContext();
+            CharSequence text = "Datos mal ingresados";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
         } else {
             // Show a progress spinner, and kick off a background task to
             // perform the user login attempt.
+            Context context = getApplicationContext();
+            CharSequence text = "Datos bien ingresados";
+            int duration = Toast.LENGTH_SHORT;
+            Toast toast = Toast.makeText(context, text, duration);
+            toast.show();
             showProgress(true);
             mAuthTask = new UserLoginTask(email, password);
             mAuthTask.execute((Void) null);
+            Intent resultIntent = new Intent();
+            resultIntent.putExtra("email",email);
+            resultIntent.putExtra("password",password);
+            setResult(Activity.RESULT_OK, resultIntent);
+            finish();
         }
     }
 
--- app/src/main/java/com/example/alfonso/lab01/MainActivity.java
+++ app/src/main/java/com/example/alfonso/lab01/MainActivity.java
@@ -1,29 +1,75 @@
 package com.example.alfonso.lab01;
 
+import android.app.Activity;
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int LOG_IN_REQUEST = 1;
+    static String EMAIL;
+    static String PASSWORD;
+    static SPManager spManager = new SPManager();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureLoginButton();
+        configureLogoutButton();
+
+        SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
+        String current_email = spManager.getEmail(this);
+        String current_password = spManager.getPassword(this);
+        if (current_password == null && current_email == null) {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivityForResult(intent, LOG_IN_REQUEST);
+        }
+        else {
+            TextView txt_email = (TextView) findViewById(R.id.text_email);
+            txt_email.append(current_email);
+            TextView txt_password = (TextView) findViewById(R.id.text_password);
+            txt_password.append(current_password);
+        }
     }
 
-    private void configureLoginButton() {
-        Button loginButton = (Button) findViewById(R.id.button_login);
-        loginButton.setOnClickListener(new View.OnClickListener() {
+    private void configureLogoutButton() {
+        final Button logoutButton = (Button) findViewById(R.id.button_logout);
+        logoutButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(MainActivity.this, LoginActivity.class));
+                spManager.removeData(view.getContext());
+                TextView ttxt = (TextView) findViewById(R.id.text_email);
+                ttxt.clearComposingText();
+                TextView passw = (TextView) findViewById(R.id.text_password);
+                passw.clearComposingText();
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivityForResult(intent, LOG_IN_REQUEST);
             }
         });
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == LOG_IN_REQUEST) {
+            if (resultCode == RESULT_OK) {
+                EMAIL = data.getStringExtra("email");
+                PASSWORD = data.getStringExtra("password");
+                TextView ttxt = (TextView) findViewById(R.id.text_email);
+                ttxt.append(EMAIL);
+                TextView passw = (TextView) findViewById(R.id.text_password);
+                passw.append(PASSWORD);
+                spManager.addData(this, EMAIL, PASSWORD);
+            }
+        }
     }
 }
 
