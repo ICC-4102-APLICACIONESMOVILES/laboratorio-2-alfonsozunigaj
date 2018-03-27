dcadda5f0472e1734f21338b552061c80072fced
Alfonso <ajzuniga1@miuandes.cl>
2018-03-27 12:20:34 -0300

    Lab02

---
+++ app/src/main/java/com/example/alfonso/lab01/ListFragment.java
@@ -0,0 +1,45 @@
+package com.example.alfonso.lab01;
+
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ListFragment extends Fragment {
+
+
+    public ListFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_list, container, false);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        ListView lista = (ListView) view.findViewById(R.id.listView_fragment);
+        ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(),
+                R.array.Info, android.R.layout.simple_list_item_1);
+        lista.setAdapter(adapter);
+
+
+    }
+}
--- app/src/main/java/com/example/alfonso/lab01/MainActivity.java
+++ app/src/main/java/com/example/alfonso/lab01/MainActivity.java
@@ -4,15 +4,25 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.ListView;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    static DrawerLayout mDrawerLayout;
 
     static final int LOG_IN_REQUEST = 1;
     static String EMAIL;
@@ -24,9 +34,75 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        configureLogoutButton();
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ActionBar actionbar = getSupportActionBar();
+        actionbar.setDisplayHomeAsUpEnabled(true);
+        actionbar.setHomeAsUpIndicator(R.drawable.ic_menu);
 
-        String current_email = spManager.getEmail(this);
+        mDrawerLayout = findViewById(R.id.drawer_layout);
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+
+                        FormFragment fragment = null;
+
+                        if(menuItem.getItemId() == R.id.nav_form) {
+                            fragment = new FormFragment();
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
+                                    fragment).commit();
+                        }
+                        else if (menuItem.getItemId() == R.id.nav_option2) {
+                            ListFragment list_fragment = new ListFragment();
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
+                                    list_fragment).commit();
+                        }
+                        else if (menuItem.getItemId() == R.id.nav_option3) {
+                            ResumeFragment resume_fragment = new ResumeFragment();
+                            getSupportFragmentManager().beginTransaction().replace(R.id.content_frame,
+                                    resume_fragment).commit();
+                        }
+                        menuItem.setChecked(true);
+                        // close drawer when item is tapped
+                        mDrawerLayout.closeDrawers();
+
+                        // Add code here to update the UI based on the item selected
+                        // For example, swap UI fragments here
+
+                        return true;
+                    }
+                });
+
+        mDrawerLayout.addDrawerListener(
+                new DrawerLayout.DrawerListener() {
+                    @Override
+                    public void onDrawerSlide(View drawerView, float slideOffset) {
+                        // Respond when the drawer's position changes
+                    }
+
+                    @Override
+                    public void onDrawerOpened(View drawerView) {
+                        // Respond when the drawer is opened
+                    }
+
+                    @Override
+                    public void onDrawerClosed(View drawerView) {
+                        // Respond when the drawer is closed
+                    }
+
+                    @Override
+                    public void onDrawerStateChanged(int newState) {
+                        // Respond when the drawer motion state changes
+                    }
+                }
+        );
+
+        //configureLogoutButton();
+
+        /*String current_email = spManager.getEmail(this);
         String current_password = spManager.getPassword(this);
         if (current_password == null && current_email == null) {
             Intent intent = new Intent(MainActivity.this, LoginActivity.class);
@@ -37,10 +113,20 @@ public class MainActivity extends AppCompatActivity {
             txt_email.setText("E-mail: " + current_email);
             TextView txt_password = (TextView) findViewById(R.id.text_password);
             txt_password.setText("Password: " + current_password);
-        }
+        }*/
     }
 
-    private void configureLogoutButton() {
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    /*private void configureLogoutButton() {
         final Button logoutButton = (Button) findViewById(R.id.button_logout);
         final SharedPreferences sharedPref = this.getPreferences(Context.MODE_PRIVATE);
         logoutButton.setOnClickListener(new View.OnClickListener() {
@@ -55,9 +141,9 @@ public class MainActivity extends AppCompatActivity {
                 startActivityForResult(intent, LOG_IN_REQUEST);
             }
         });
-    }
+    }*/
 
-    @Override
+    /*@Override
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
         if (requestCode == LOG_IN_REQUEST) {
             if (resultCode == RESULT_OK) {
@@ -70,6 +156,6 @@ public class MainActivity extends AppCompatActivity {
                 spManager.addData(this, EMAIL, PASSWORD);
             }
         }
-    }
+    }*/
 }
 
